6d87695aea43b7a10b479dd5fd77ce4ed19abbad
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-09-26 06:51:04 +0100

    added base stylings

---
+++ src/components/App/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './app.module';
+
+export default function App({}) {
+	return (
+		<div className={classes.dark}>
+			<h1>Welcome to country wiki</h1>
+		</div>
+	);
+}
